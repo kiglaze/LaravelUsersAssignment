bcf2ccdb046c520e7ca65157af09359bfe54be23
Iris Glaze <iris@iriss-mbp.attlocal.net>
2019-12-03 04:11:31 -0500

    Added use of a laravel collective with Composer that allows me to use form-model binding and the relevant syntax. Used this new syntax to add the start of a form for the create user page. Form submission still needs to have a working relevant POST route linked up with it.

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,4 +2,20 @@
 
 @section('content')
     <h1>Create User</h1>
+
+    {{ Form::open() }}
+        <div>
+            {{ Form::label('name', 'Name: ') }}
+            {{ Form::text('name') }}
+        </div>
+
+        <div>
+            {{ Form::label('email', 'Email: ') }}
+            {{ Form::text('email') }}
+        </div>
+
+        <div>
+            {{ Form::submit('Add New User') }}
+        </div>
+    {{ Form::close() }}
 @endsection
