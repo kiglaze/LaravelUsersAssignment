d6723b007c962aa69f836ca897ab21bf1ecd6129
Iris Glaze <iris@iriss-mbp.attlocal.net>
2019-12-03 02:06:08 -0500

    Created Users controller. So far I'm only using it to show the users list view. The list only shows static user data right now; this has to be made into dynamic data.

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,4 +2,7 @@
 
 @section('content')
     <h1>Users List</h1>
+    @foreach($users as $user)
+        <p>{{ $user }}</p>
+    @endforeach
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/users', function () {
-    return view('users.index');
-});
+Route::get('/users', 'UsersController@index')->name('users.index');
