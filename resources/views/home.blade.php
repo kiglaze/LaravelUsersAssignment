d85cd32b6ade5e513ae4c673fef8f98ab4bc6801
Iris Glaze <iris@iriss-mbp.attlocal.net>
2019-12-04 18:11:49 -0500

    Tweaked frontend template for update user form in order to make the styling be the same as the user registration page form.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,40 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
                     <h1>Edit User Info</h1>
-
                     {{ Form::model( Auth::user(), ['method' => 'PATCH', 'route' => 'users.updateCurrent'] ) }}
-                        <div class="field">
-                            {{ Form::label('name', 'Name: ') }}
-                            <div class="control">
-                                {{ Form::text('name') }}
+                        @csrf
+
+                        <div class="form-group row">
+                            {{ Form::label('name', 'Name: ', ['class' => "col-md-4 col-form-label text-md-right"]) }}
+
+                            <div class="col-md-6">
+                                {{ Form::text('name', null, ['class' => 'form-control', 'name' => 'name']) }}
+
                                 <p class="help is-danger">{{ $errors->first('name') }}</p>
                             </div>
                         </div>
 
-                        <div class="field">
-                            {{ Form::label('email', 'Email: ') }}
-                            <div class="control">
-                                {{ Form::text('email') }}
+                        <div class="form-group row">
+                            {{ Form::label('email', 'Email: ', ['class' => "col-md-4 col-form-label text-md-right"]) }}
+
+                            <div class="col-md-6">
+                                {{ Form::text('email', null, ['class' => 'form-control', 'name' => 'email']) }}
+
                                 <p class="help is-danger">{{ $errors->first('email') }}</p>
                             </div>
                         </div>
 
-                        <div>
-                            {{ Form::submit('Update User Info') }}
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                {{ Form::submit('Update User Info', ['class' => "btn btn-primary"]) }}
+                            </div>
                         </div>
                     {{ Form::close() }}
                 </div>
