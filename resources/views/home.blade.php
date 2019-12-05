d0c8685e6fd5358bccbe4da001b0616e2b5044e4
Iris Glaze <iris@iriss-mbp.attlocal.net>
2019-12-05 15:59:37 -0500

    Added address field to Edit User Infromation form. Made them be pre-filled with form-model binding. However, the update info button does not work for the address data yet.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,6 +33,76 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            {{ Form::label('street_address', 'Street Address: ', ['class' => "col-md-4 col-form-label text-md-right"]) }}
+
+                            <div class="col-md-6">
+                                {{ Form::text('address[street_address]', null, ['class' => 'form-control', 'name' => 'street_address']) }}
+
+                                @error('street_address')
+                                <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            {{ Form::label('city', 'City: ', ['class' => "col-md-4 col-form-label text-md-right"]) }}
+
+                            <div class="col-md-6">
+                                {{ Form::text('address[city]', null, ['class' => 'form-control', 'name' => 'city']) }}
+
+                                @error('city')
+                                <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            {{ Form::label('region', 'State / Region: ', ['class' => "col-md-4 col-form-label text-md-right"]) }}
+
+                            <div class="col-md-6">
+                                {{ Form::text('address[region]', null, ['class' => 'form-control', 'name' => 'region']) }}
+
+                                @error('region')
+                                <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            {{ Form::label('country', 'Country: ', ['class' => "col-md-4 col-form-label text-md-right"]) }}
+
+                            <div class="col-md-6">
+                                {{ Form::text('address[country]', null, ['class' => 'form-control', 'name' => 'country']) }}
+
+                                @error('country')
+                                <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            {{ Form::label('postcode', 'Zip Code: ', ['class' => "col-md-4 col-form-label text-md-right"]) }}
+
+                            <div class="col-md-6">
+                                {{ Form::text('address[postcode]', null, ['class' => 'form-control', 'name' => 'postcode']) }}
+
+                                @error('postcode')
+                                <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                @enderror
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 {{ Form::submit('Update User Info', ['class' => "btn btn-primary"]) }}
