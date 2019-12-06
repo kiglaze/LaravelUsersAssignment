1a412ce0a7809ea66d1a7d7f896920adae36b3a7
Iris Glaze <iris@iriss-mbp.attlocal.net>
2019-12-06 17:17:56 -0500

    Made address display on users list page be more in an address format.

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,6 +9,13 @@
                     <li {{ ((auth()->check()) && (auth()->user()->getAuthIdentifier() === $user->id)) ? "class = current" : "" }}>
                         <p>{{ $user->name }}</p>
                         <p>{{ $user->email }}</p>
+                        @if($user->address)
+                            <div class="address">
+                                <p>{{ $user->address->street_address }}</p>
+                                <p>{{ $user->address->city }}, {{ $user->address->region }}</p>
+                                <p>{{ $user->address->country }}, {{ $user->address->postcode }}</p>
+                            </div>
+                        @endif
                     </li>
                 @endforeach
             </ul>
