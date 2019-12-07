4a8b76e6b6eceb7077f85d9d26d64d2c60aca5f0
Iris Glaze <iris@iriss-mbp.attlocal.net>
2019-12-07 18:09:56 -0500

    spruced up a couple things on the default page and changed the redirect to page after login or registration.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,12 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">Company</a>
+                @auth
+                    <a class="navbar-brand" href="{{ url('/') }}">{{ __("Company Users List") }}</a>
+                @else
+                    <a class="navbar-brand" href="{{ url('/') }}">{{ __("Company") }}</a>
+                @endauth
+
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
